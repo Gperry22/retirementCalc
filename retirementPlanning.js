20b11d18847e00e7f0b1454b3130f31f3434da38
Gabriel Perry <gperry22@gmail.com>
2019-05-24 10:38:02 -0400

    Added 401k Matching

--- retirementPlanning.js
+++ retirementPlanning.js
@@ -9,7 +9,6 @@ try {
 }
 
 var total = 0;
-var initInvested = 0
 
 
 inquirer
@@ -62,31 +61,44 @@ inquirer
                 return 'Please enter a number';
             }
         },
+        {
+            type: "input",
+            name: "compMatch",
+            message: "If this is for a 401(k) and you company offers matching. Please enter the matching amount here. Otherwise enter 0.",
+            validate: function (input) {
+                var pass = /^[0-9][A-Za-z0-9 -]*$/.test(input);
+                if (pass) {
+                    return true;
+                }
+                return 'Please enter a number';
+            }
+        },
 
     ]).then((answers) => {
 
         var initInvested = parseFloat(answers.initialInvestment)
         var yearlyAmountToInvest = parseFloat(answers.amountToInvest);
         var numOfYearsToInvest = parseFloat(answers.numOfYears)
-        var roi = parseFloat(answers.return)
-        var expectReturnOnInvestment = roi / 100
-        printStats(initInvested, yearlyAmountToInvest, numOfYearsToInvest, expectReturnOnInvestment)
+        var expectReturnOnInvestment = parseFloat(answers.return)/100
+        var companyMatching = parseFloat(answers.compMatch)/100
+        printStats(initInvested, yearlyAmountToInvest, numOfYearsToInvest, expectReturnOnInvestment, companyMatching)
 
 
     })
 
 
 
-function printStats(initInvested, yati, noyti, eroi) {
+function printStats(initInvested, yati, noyti, eroi, compM) {
 
     console.log(initInvested);
     var initial = initInvested
+    var compAmount = yati * compM
 
     for (let i = 0; i < noyti; i++) {
 
         var amount = initial + yati + total;
         var gains = amount * eroi;
-        total = amount + gains;
+        total = amount + compAmount + gains;
         initial = 0;
         var yearNum = i + 1;
 
@@ -94,9 +106,10 @@ function printStats(initInvested, yati, noyti, eroi) {
         var b = gains.toFixed(2)
         var c = total.toFixed(2)
         var d = yati.toFixed(2)
+        var e = compAmount.toFixed(2)
 
         fs.appendFileSync('retirement.txt', "Year " + yearNum + '\n');
-        fs.appendFileSync('retirement.txt', "Amount: $" + a + "||   Amount Invested: $" + d + "||   Expected Return: $" + b + "||    Total: $" + c + '\n');
+        fs.appendFileSync('retirement.txt', "Amount: $" + a + "||   Amount Invested: $" + d + "||   Expected Return: $" + b + "||   Company Matching: $" + e +  "||    Total: $" + c + '\n');
     }
 
 }
